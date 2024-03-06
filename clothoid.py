08ce809d8768f064b03f58f1eb1f6b3c6c1cfd28
Njål Ingersønn Sørland <njaalis2000@gmail.com>
2024-03-06 22:57:13 +0100

    update clothoid

--- clothoid.py
+++ clothoid.py
@@ -6,13 +6,20 @@ def plan_clothoid(start_x, start_y, start_angle, end_x, end_y, end_angle):
     clothoid = Clothoid.G1Hermite(start_x, start_y, start_angle, end_x, end_y, end_angle)
     return clothoid
 
-def plot_clothoid(clothoid: Clothoid):
+def plot_clothoid(clothoid: Clothoid, start, end):
     x, y = clothoid.SampleXY(100)
     plt.plot(x, y)
+    plt.scatter([start[0], end[0]], [start[1], end[1]], color=['green', 'red'])
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.title('Clothoid Path Planning')
+    plt.grid(True)
     plt.axis('equal')
     plt.show()
 
 if __name__ == "__main__":
-    _clothoid = plan_clothoid(0, 0, np.pi, 10, 10, np.pi)
-    plot_clothoid(_clothoid)
+    start = (0, 0, 0)
+    end = (1, 0, -np.pi/2)
+    _clothoid = plan_clothoid(*start, *end)
+    plot_clothoid(_clothoid, start, end)
 
